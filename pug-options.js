71fcf1780cf64f8b9f7a22355d01961a681708ed
Francisco Ryan Tolmasky I <tolmasky@gmail.com>
2018-07-31 18:33:41 -0700

    Embed a working example server (#38)
    * Use endpoint.
    
    Reviewed by @tolmasky.
    
    * Fix remove alert.
    
    Reviewed by @tolmasky.
    
    * Change file.
    
    Reviewed by @tolmasky.
    
    * Fix conflict.
    
    Reviewed by @tolmasky.
    
    * Add highlight.js to package.json.
    
    Reveiwed by @tolmasky.
    
    * Update index.html. Fix code.
    
    Reviewed by @tolmasky.
    
    * Update index.html.
    
    Reviewed by @tolmasky.
    
    * Re-remove highlight.js clientside.
    
    Reviewed by @tolmasky.

---
+++ pug-options.js
@@ -0,0 +1,71 @@
+{
+    filters:
+    {
+        markdown: function (text)
+        {
+            var marked = require("marked");
+            var renderer = new marked.Renderer();
+            var highlight = require('highlight.js').highlight;
+            var runkitRegExp = /^<!--\s*runkit:endpoint((.|\n)*)-->(.|\n)*$/;
+            var runkitContext = { options: '{}', activated: false };
+
+            renderer.code = function(code, lang, escaped) {
+
+                if (!global.exampleCount)
+                    global.exampleCount = 0;
+
+                var out = highlight(lang, code).value || code;
+                var escaped = out !== code ? out : escapeCode(out, true);
+                var id = "example-" + (global.exampleCount++);
+
+                var script = runkitContext.activated ? "<script>(" + endpoint + ")(\"" + id + "\")</script>" : "";
+
+                runkitContext.activated = false;
+
+                return "<div id = \"" + id + "\"><pre><code class = \"lang-" + lang + "\">" + escaped + "</code></pre></div>" + script;
+            };
+
+            renderer.html = function(text) {
+                var result = runkitRegExp.exec(text);
+
+                if (!result) return text;
+
+                runkitContext.activated = true;
+
+                return text;
+            };
+
+            return marked(text, { renderer: renderer });
+
+            function endpoint(id, count) {
+            
+                if (!window.RunKit)
+                    if (typeof count === "undefined" || count < 20)
+                        return setTimeout(endpoint, 500, id, count || 0 + 1);
+                    else
+                        return;
+
+                var parent = document.getElementById(id);
+                var source = parent.textContent;
+                
+                parent.innerHTML = "";
+
+                RunKit.createNotebook({
+                    element: parent,
+                    nodeVersion: "8.x.x",
+                    source: source,
+                    mode: "endpoint"
+                });
+            }
+            
+            function escapeCode(code) {
+              return code
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;');
+            }
+        }
+    }
+}
